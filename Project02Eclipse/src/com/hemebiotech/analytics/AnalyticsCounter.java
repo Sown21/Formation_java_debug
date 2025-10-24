f929714924a512036363811497c03f6d6d3a3ef1
Sown21 <Sown57@gmail.com>
2025-10-24 14:10:02 +0200

    refactor AnalyticsCounter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,32 +4,32 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
-	
-	public static void main(String args[]) throws Exception {
-		Map<String, Integer> symptomCounts = new TreeMap<>();
+    private final ISymptomReader reader;
+    private final ISymptomWriter writer;
 
-		try (BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if (!line.isEmpty()) {
-                    symptomCounts.put(line, symptomCounts.getOrDefault(line, 0) + 1);
-                }
-            }
-		} catch (IOException e) {
-            System.err.println("Error reading symptoms file: " + e.getMessage());
-        }
+    public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
 
-        FileWriter writer = new FileWriter ("result.out");
-        for (Map.Entry<String, Integer> entry : symptomCounts.entrySet()) {
-            writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
+    public List<String> getSymptoms() {
+        return reader.GetSymptoms();
+    }
+
+    public Map<String, Integer> countSymptoms(List<String> symptoms) {
+        Map<String, Integer> count = new TreeMap<>();
+        for (String symptom : symptoms) {
+            count.put(symptom, count.getOrDefault(symptom, 0) + 1);
         }
-		writer.close();
-	}
+        return count;
+    }
+
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+        writer.writeSymptoms(symptoms);
+    }
 }
