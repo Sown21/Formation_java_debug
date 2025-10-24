a80b1a748d7f6860c129638d4859988dde0687ae
Sown21 <Sown57@gmail.com>
2025-10-24 13:53:19 +0200

    add WriteSymptomDataToFile class

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -1,4 +1,26 @@
 package com.hemebiotech.analytics;
 
-public class WriteSymptomDataToFile {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class WriteSymptomDataToFile implements ISymptomWriter {
+    private String finalPath;
+
+    public WriteSymptomDataToFile(String finalPath) {
+        this.finalPath = finalPath;
+    }
+
+    @Override
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+        try {
+            FileWriter writer = new FileWriter (finalPath);
+            for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
+                writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
